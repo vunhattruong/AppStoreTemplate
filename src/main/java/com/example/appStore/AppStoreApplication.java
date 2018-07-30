43e77fd69ec8be4aef396755aec2ad5dd1ad1f1b
Truong Vu <vunhtruong@gmail.com>
2018-07-30 14:46:08 +0700

    First commit

---
+++ src/main/java/com/example/appStore/AppStoreApplication.java
@@ -0,0 +1,12 @@
+package com.example.appStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStoreApplication.class, args);
+	}
+}
